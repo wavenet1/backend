981e277786fb19ceaec9151a9a5814014140d9f3
appledefender <appledefender@gmail.com>
2024-01-26 06:13:26 +0500

    changes

--- medusa-config.js
+++ medusa-config.js
@@ -22,8 +22,7 @@ try {
 } catch (e) {}
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS = "/https:\/\/*/"
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
@@ -86,3 +85,4 @@ module.exports = {
   plugins,
   modules,
 };
+
